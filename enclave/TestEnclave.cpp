d435620cbf20b173562ab0fe0d8fda10c40cb309
rcosta-git <robertcosta378@gmail.com>
2022-04-24 15:02:35 -0400

    First commit

---
+++ enclave/TestEnclave.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#include <stdio.h>      /* vsnprintf */
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "TestEnclave.h"
+#include "TestEnclave_t.h"  /* print_string */
+#include "tSgxSSL_api.h"
+
+#include <openssl/ec.h>
+#include <openssl/bn.h>
+#include <openssl/rsa.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
+
+#define ADD_ENTROPY_SIZE	32
+
+/* 
+ * printf: 
+ *   Invokes OCALL to display the enclave buffer to the terminal.
+ */
+void printf(const char *fmt, ...)
+{
+    char buf[BUFSIZ] = {'\0'};
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, BUFSIZ, fmt, ap);
+    va_end(ap);
+    uprint(buf);
+}
+
+typedef void CRYPTO_RWLOCK;
+
+struct evp_pkey_st {
+    int type;
+    int save_type;
+    int references;
+    const EVP_PKEY_ASN1_METHOD *ameth;
+    ENGINE *engine;
+    union {
+        char *ptr;
+# ifndef OPENSSL_NO_RSA
+        struct rsa_st *rsa;     /* RSA */
+# endif
+# ifndef OPENSSL_NO_DSA
+        struct dsa_st *dsa;     /* DSA */
+# endif
+# ifndef OPENSSL_NO_DH
+        struct dh_st *dh;       /* DH */
+# endif
+# ifndef OPENSSL_NO_EC
+        struct ec_key_st *ec;   /* ECC */
+# endif
+    } pkey;
+    int save_parameters;
+    STACK_OF(X509_ATTRIBUTE) *attributes; /* [ 0 ] */
+    CRYPTO_RWLOCK *lock;
+} /* EVP_PKEY */ ;
+
+RSA *global_keypair = NULL;
+EVP_PKEY *global_evp_pkey = NULL;
+
+int t_gen_keys(unsigned char *buf)
+{
+	BIGNUM *bn = BN_new();
+	if (bn == NULL) {
+            printf("BN_new failure: %ld\n", ERR_get_error());
+            return 0;
+	}
+	int ret = BN_set_word(bn, RSA_F4);
+        if (!ret) {
+            printf("BN_set_word failure\n");
+            return 0;
+	}
+	
+	global_keypair = RSA_new();
+	if (global_keypair == NULL) {
+            printf("RSA_new failure: %ld\n", ERR_get_error());
+            return 0;
+	}
+	ret = RSA_generate_key_ex(global_keypair, 4096, bn, NULL);
+	if (!ret) {
+            printf("RSA_generate_key_ex failure: %ld\n", ERR_get_error());
+            return 0;
+	}
+
+	global_evp_pkey = EVP_PKEY_new();
+	if (global_evp_pkey == NULL) {
+            printf("EVP_PKEY_new failure: %ld\n", ERR_get_error());
+            return 0;
+	}
+	EVP_PKEY_assign_RSA(global_evp_pkey, global_keypair);
+
+        int len = 0;
+        unsigned char *secureBuf = NULL;
+        len = i2d_RSAPublicKey(global_keypair, &secureBuf);
+        memcpy(buf, secureBuf, len);
+        return len;
+}
+
---
+++ enclave/TestEnclave_t.h
@@ -0,0 +1,36 @@
+#ifndef TESTENCLAVE_T_H__
+#define TESTENCLAVE_T_H__
+
+#include <stdint.h>
+#include <wchar.h>
+#include <stddef.h>
+#include "sgx_edger8r.h" /* for sgx_ocall etc. */
+
+
+#include <stdlib.h> /* for size_t */
+
+#define SGX_CAST(type, item) ((type)(item))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int t_gen_keys(unsigned char* pbuf);
+
+sgx_status_t SGX_CDECL uprint(const char* str);
+sgx_status_t SGX_CDECL usgx_exit(int reason);
+sgx_status_t SGX_CDECL u_sgxssl_ftime(void* timeptr, uint32_t timeb_len);
+sgx_status_t SGX_CDECL sgx_oc_cpuidex(int cpuinfo[4], int leaf, int subleaf);
+sgx_status_t SGX_CDECL sgx_thread_wait_untrusted_event_ocall(int* retval, const void* self);
+sgx_status_t SGX_CDECL sgx_thread_set_untrusted_event_ocall(int* retval, const void* waiter);
+sgx_status_t SGX_CDECL sgx_thread_setwait_untrusted_events_ocall(int* retval, const void* waiter, const void* self);
+sgx_status_t SGX_CDECL sgx_thread_set_multiple_untrusted_events_ocall(int* retval, const void** waiters, size_t total);
+sgx_status_t SGX_CDECL pthread_wait_timeout_ocall(int* retval, unsigned long long waiter, unsigned long long timeout);
+sgx_status_t SGX_CDECL pthread_create_ocall(int* retval, unsigned long long self);
+sgx_status_t SGX_CDECL pthread_wakeup_ocall(int* retval, unsigned long long waiter);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
